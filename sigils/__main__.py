698ecb2e92ef28515d2bc05829f6d4c736d012cb
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-15 23:22:51 -0600

    build

--- sigils/__main__.py
+++ sigils/__main__.py
@@ -1,50 +1,42 @@
 import argparse
-import json
-import tomllib as toml
 import sys
-import unittest
-from collections import ChainMap
 from sigils import Sigil
-
-def flatten(d, parent_key='', sep='.'):
-    items = []
-    for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, dict):
-            items.extend(flatten(v, new_key, sep=sep).items())
-        else:
-            items.append((new_key, v))
-    return dict(items)
 
 def main():
     parser = argparse.ArgumentParser(description="Interpolate sigils in text.")
     parser.add_argument("text", nargs='?', default="", help="Text with sigils.")
-    parser.add_argument("--context", "-c", action='append', help="JSON/TOML file(s) to provide context for interpolation.")
+    parser.add_argument("--context", "-c", help="JSON/TOML file to provide context for interpolation.")
+    parser.add_argument("--target", "-t", help="Target key in the context for interpolation.")
     parser.add_argument("--execute", "-e", action='store_true', help="Execute callable values.")
     parser.add_argument("--max-depth", "-d", type=int, default=6, help="Maximum recursion depth.")
-    parser.add_argument("--tests", action='store_true', help="Run tests.")
+    parser.add_argument("--test", action='store_true', help="Run tests.")
     args = parser.parse_args()
 
-    if args.tests:
+    if args.test:
+        import unittest
         from .tests import TestSigil
         suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestSigil)
         unittest.TextTestRunner().run(suite)
     else:
-        context_files = args.context if args.context else []
-        contexts = []
-        for context_file in context_files:
+        context_file = args.context
+        if context_file:
             with open(context_file, 'r') as f:
                 if context_file.endswith('.json'):
-                    context = flatten(json.load(f))
+                    import json
+                    context = json.load(f)
                 elif context_file.endswith('.toml'):
-                    context = flatten(toml.load(f))
+                    import tomllib as toml
+                    context = toml.load(f)
                 else:
                     print(f"Unsupported file format: {context_file}", file=sys.stderr)
                     sys.exit(1)
-                contexts.append(context)
+        else:
+            context = {}
 
-        chain_map = ChainMap(*reversed(contexts))  # Last context takes precedence
-        print(Sigil(args.text) % chain_map)  
+        if args.target:
+            context = context.get(args.target)
+
+        print(Sigil(args.text).interpolate(context)) 
 
 if __name__ == "__main__":
     main()
