562fd67bee96ccb03ba44ed4cbd45dd0444477bb
arthexis <arthexis@gmail.com>
2026-09-07 15:32:01 -0600

    Export safe namespace API

--- sigils/__init__.py
+++ sigils/__init__.py
@@ -1,5 +1,6 @@
 from .context import Context
+from .namespace import NamespaceProvider, SafeNamespace
 from .secret import Secret
 from .sigil import Sigil
 
-__all__ = ["Context", "Secret", "Sigil"]
+__all__ = ["Context", "NamespaceProvider", "SafeNamespace", "Secret", "Sigil"]
